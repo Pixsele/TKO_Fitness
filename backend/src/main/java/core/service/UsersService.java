f0f82a69416eae5522ce66136912355accaac638
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2025-03-01 14:23:59 +0400

    aaa?

---
+++ backend/src/main/java/core/service/UsersService.java
@@ -0,0 +1,20 @@
+package core.service;
+
+
+import core.database.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public
+
+
+}
